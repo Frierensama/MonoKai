81cba244da1661142fde089cf43ec5f5447a79d5
code:/02 <rudeusgreyrat.zeta002@gmail.com>
2026-08-21 12:41:27 +0530

    messages state debug
    [humanmessage , aimessage]
    it was like
    [humanmessage, {aimessage}, human..]
    errors while accessing messages through iteration

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -28,7 +28,7 @@ def chat_node(state:ChatSchema):
     messages = state['messages']
     response = google_model.invoke(messages) 
     # response itself is Ai message, so, when i comeback again, dont need to extract content and send as AImessage
-    return {'messages':response}
+    return {'messages':[response]}
 
 
 checkpointer = InMemorySaver()
