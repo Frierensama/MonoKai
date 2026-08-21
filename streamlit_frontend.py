f4c5b6df45853a2bfc98890fcd24ce2259584a3f
code:/02 <rudeusgreyrat.zeta002@gmail.com>
2026-08-21 11:38:19 +0530

    multiple chat threads
    didn't implement resume chat yet.
    have to store the chat_messages to that thread id.

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,12 +1,49 @@
 import streamlit as st
 from langgraph_backend import workflow
 from langchain_core.messages import HumanMessage, AIMessage
+import uuid
 
-CONFIG = {'configurable':{'thread_id':'thread-002'}}
-st.set_page_config(page_title='Saturn-X',page_icon='🪐')
+st.set_page_config(page_title='Tsuki',page_icon='🍥')
+
+# --------------------------- utilities  -----------------------------
+def gen_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+def new_chat():
+    new_thread_id = gen_thread_id()
+    add_thread_id(new_thread_id)
+    st.session_state['thread_id'] = new_thread_id
+    st.session_state['chat_messages'] = []
+
+def add_thread_id(thread):
+    if thread not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread)
+
+#----------------------------- session setup  -----------------------------------
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = gen_thread_id()
+    st.session_state['chat_threads'] = [st.session_state['thread_id']]
 
 if 'chat_messages' not in st.session_state:
     st.session_state['chat_messages'] = []
+
+
+
+CONFIG = {'configurable':{'thread_id':st.session_state['thread_id']}}
+# ---------------------------- sidebar  -----------------------------------
+
+st.sidebar.title('`Clove 1.1`',width='stretch')
+
+if st.sidebar.button('New Chat'):
+    new_chat()
+
+st.sidebar.header('My Chats')
+
+for chat_thread in st.session_state['chat_threads']:
+    selected_chat_thread = st.sidebar.button(str(chat_thread))
+
+# --------------------------- User Interaction ------------------------------------
 
 for message in st.session_state['chat_messages']:
     with st.chat_message(message['role']):
