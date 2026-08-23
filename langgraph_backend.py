c738fe761b7dd6d380f5a6f4018a359826f5e348
code:/02 <rudeusgreyrat.zeta002@gmail.com>
2026-08-23 16:17:46 +0530

    Update langgraph_backend.py

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,18 +1,53 @@
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
+from langchain.tools import tool
+from langchain_community.tools import DuckDuckGoSearchRun
 
 from langgraph.graph.state import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.checkpoint.sqlite import SqliteSaver
 import sqlite3
 
-from typing import Annotated, TypedDict, List, Dict
+from typing import Annotated, TypedDict, List, Literal
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
+
+# TOOLS
+search_tool = DuckDuckGoSearchRun()
+
+@tool
+def calculator(first_num:float, second_num:float,operator:Literal['add','sub','mul','div'])->dict:
+    """
+    A function to perform math operations - add, subract, multiply, divide
+    """
+    result = 0.0
+    if operator == 'add':
+        result = first_num + second_num
+    elif operator == 'sub':
+        result = first_num - second_num
+    elif operator == 'mul':
+        result = first_num * second_num
+    elif operator == 'div':
+        if second_num == 0:
+            return {'error':'cannot divide with zero'}
+        else:
+            result = first_num / second_num
+    else:
+        return {'error':f'unsupported operation{operator}'}
+
+    return {'first_num':first_num,'second_num':second_num,'operator':operator,'result':result}
+
+
+    
+
+# LLM
 llm_endpoint = HuggingFaceEndpoint(
     repo_id=os.getenv('hf_model'),
     provider='together'
@@ -21,6 +56,9 @@ model = ChatHuggingFace(llm=llm_endpoint)
 
 google_model = ChatGoogleGenerativeAI(model='gemini-3.1-flash-lite')
 
+
+
+# STATE GRAPH -- WORKFLOW
 class ChatSchema(TypedDict):
     topic:str
     messages:Annotated[List[BaseMessage], add_messages]
@@ -46,6 +84,8 @@ stategraph.add_edge('chat_node',END)
 workflow = stategraph.compile(checkpointer=checkpointer)
 
 
+
+# UTILITY FUNCS
 def get_all_thread_ids():
     thread_ids = set()
     for checkpoint in checkpointer.list(None):
